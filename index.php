ecc21c820675bafa3a705ac322da8f58c39b7c7f
elmas-euler <59921688+elmas-euler@users.noreply.github.com>
2020-09-07 16:16:46 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $color = "red";
+  echo "The color is " .$color. "<br>";
+  echo "Hello, World!";
+  ?>
+</body>
+</html>
